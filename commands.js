a6521062bddf11e1528286bace75858da6dd0749
rash2x <rash2x@gmail.com>
2018-06-17 01:33:56 +0800

    setup to netlify

--- commands.js
+++ commands.js
@@ -1,4 +1,3 @@
-const path = require('path');
 const Accounts = require('./accounts');
 const CommandStore = require('./commandStore');
 const ACTIONS = require('./actions');
@@ -7,7 +6,83 @@ const MESSAGES = require('./messages');
 
 const GROUP_ID = -232611552;
 
-module.exports = class Commands {
+const _commands = {
+    'START': (bot, msg) => {
+        CommandStore.add(msg.chat.id, msg.text);
+
+        Accounts.check(msg).then(status => {
+            if (status) {
+                bot.sendMessage(msg.chat.id, MESSAGES['START'], {
+                    'reply_markup': {
+                        keyboard: KEYBOARDS['START']
+                    }
+                });
+            } else {
+                bot.sendMessage(msg.chat.id, MESSAGES['AUTH'], {
+                    'reply_markup': {
+                        keyboard: KEYBOARDS['AUTH']
+                    }
+                });
+            }
+        });
+    },
+    'AUTH': (bot, msg) => {
+        CommandStore.add(msg.chat.id, msg.text);
+
+        bot.sendMessage(msg.chat.id, MESSAGES['AUTH'], {
+            'reply_markup': {
+                keyboard: KEYBOARDS['START']
+            }
+        });
+    },
+    'CALL_TAXI': (bot, msg) => {
+        CommandStore.add(msg.chat.id, msg.text);
+
+        bot.sendMessage(msg.chat.id, MESSAGES['CHAT_TAXI'], {
+            'reply_markup': {
+                keyboard: KEYBOARDS['TAXI']
+            }
+        });
+    },
+    'CANCEL_TAXI': (bot, msg) => {
+        CommandStore.add(msg.chat.id, msg.text);
+
+        bot.sendMessage(msg.chat.id, MESSAGES['CANCEL_TAXI'], {
+            'reply_markup': {
+                keyboard: KEYBOARDS['START']
+            }
+        });
+    },
+    'RENT_BIKE': (bot, msg) => {
+        CommandStore.add(msg.chat.id, msg.text);
+
+        bot.sendMessage(msg.chat.id, MESSAGES['RENT_BIKE'], {
+            'reply_markup': {
+                keyboard: KEYBOARDS['BIKES']
+            }
+        });
+    },
+    'INSTRUCTOR': (bot, msg) => {
+        CommandStore.add(msg.chat.id, msg.text);
+
+        bot.sendMessage(msg.chat.id, MESSAGES['INSTRUCTOR'], {
+            'reply_markup': {
+                keyboard: KEYBOARDS['INSTRUCTOR']
+            }
+        });
+    },
+    'BACK': (bot, msg) => {
+        CommandStore.add(msg.chat.id, msg.text);
+
+        bot.sendMessage(msg.chat.id, 'Возвращаем обратно', {
+            'reply_markup': {
+                keyboard: KEYBOARDS['START']
+            }
+        });
+    }
+};
+
+class Commands {
     callTaxi(bot, msg) {
         Accounts.get(msg).then(account => {
             if (!account) return;
@@ -77,18 +152,22 @@ module.exports = class Commands {
     };
 
     send(bot, msg) {
-        const lastCommand = CommandStore.get(msg.chat.id);
-
-        if (!lastCommand) return;
-
         const bikes = [].concat(KEYBOARDS['BIKES'][0], KEYBOARDS['BIKES'][1]);
         const isBike = bikes.findIndex(name => name === msg.text);
         const isAuth = msg.hasOwnProperty('contact');
         const isLocation = msg.hasOwnProperty('location');
         const isCallTaxi = msg.text === ACTIONS['CALL_TAXI'];
         const isCancelTaxi = msg.text === ACTIONS['CANCEL_TAXI'];
-        const isRentBike = lastCommand.text === ACTIONS['RENT_BIKE'];
+        const isRentBike = msg.text === ACTIONS['RENT_BIKE'];
         const isCallInstructor = msg.text === ACTIONS['CALL_INSTRUCTOR'];
+
+        if (msg.text === ACTIONS['START']) _commands['START'](bot, msg);
+        if (msg.text === ACTIONS['AUTH']) _commands['AUTH'](bot, msg);
+        if (msg.text === ACTIONS['CALL_TAXI']) _commands['CALL_TAXI'](bot, msg);
+        if (msg.text === ACTIONS['CANCEL_TAXI']) _commands['CANCEL_TAXI'](bot, msg);
+        if (msg.text === ACTIONS['RENT_BIKE']) _commands['RENT_BIKE'](bot, msg);
+        if (msg.text === ACTIONS['INSTRUCTOR']) _commands['INSTRUCTOR'](bot, msg);
+        if (msg.text === ACTIONS['BACK']) _commands['BACK'](bot, msg);
 
         if (isBike >= 0) this.rentBikeSent(bot, msg);
         if (isAuth) this.auth(bot, msg);
@@ -100,4 +179,6 @@ module.exports = class Commands {
 
         CommandStore.add(msg.chat.id, msg.text);
     }
-};
+}
+
+module.exports = Commands;
