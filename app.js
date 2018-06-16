1cd25a7e19e5a8d11736881f17e68ca7c2631df2
rash2x <rash2x@gmail.com>
2018-06-17 01:50:26 +0800

    export to exports

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require('./configs/db');
 require('dotenv').load();
 
-module.export = function(event, context, callback) {
+module.exports = function(event, context, callback) {
     const TelegramBot = require('node-telegram-bot-api');
     const token = process.env.telegramToken ? process.env.telegramToken : '618660015:AAE6Kll-1niJgZwWsPnsqVgi8VGw194pV6Y';
     const bot = new TelegramBot(token);
