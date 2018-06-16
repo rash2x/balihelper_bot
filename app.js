c450364234552570e5a45fb730765f7e0a856cfc
rash2x <rash2x@gmail.com>
2018-06-17 01:49:16 +0800

    setup to netlify

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 require('./configs/db');
 require('dotenv').load();
 
-const TelegramBot = require('node-telegram-bot-api');
-const token = process.env.telegramToken ? process.env.telegramToken : '618660015:AAE6Kll-1niJgZwWsPnsqVgi8VGw194pV6Y';
-const bot = new TelegramBot(token, {polling: true});
+module.export = function(event, context, callback) {
+    const TelegramBot = require('node-telegram-bot-api');
+    const token = process.env.telegramToken ? process.env.telegramToken : '618660015:AAE6Kll-1niJgZwWsPnsqVgi8VGw194pV6Y';
+    const bot = new TelegramBot(token);
 
-let Commands = require('./commands');
-Commands = new Commands;
+    let Commands = require('./commands');
+    Commands = new Commands;
 
-bot.on('message', (msg) => {
-    Commands.send(bot, msg);
-});
-
-return true;
+    bot.on('message', (msg) => {
+        Commands.send(bot, msg);
+    });
+};
--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-require('./app');
+const app = require('./app');
+
+app();
